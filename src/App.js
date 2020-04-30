1d5f34e2bbd20c8f426a27e29c0116ff81bc9997
prithvi16 <pvsukale@gmail.com>
2020-04-29 18:54:18 -0700

    Minimal working version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    console.log("coming to constructor");
+    this.handleClick = this.handleClick.bind(this);
+    this.displayLocationInfo = this.displayLocationInfo.bind(this);
+    this.state = { userLongitude: 1, userLatitude: 1 };
+  }
+  handleClick(e) {
+    e.preventDefault();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
+    } else {
+      console.log("We don't have geolocation");
+    }
+  }
+
+  displayLocationInfo(position) {
+    const lng = position.coords.longitude;
+    const lat = position.coords.latitude;
+    this.setState({ userLatitude: lat, userLongitude: lng });
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>COVID 19 testing locations</h1>
+        <button onClick={this.handleClick}>hello</button>
+        <h2>
+          Your location is {this.state.userLongitude} and{" "}
+          {this.state.userLatitude}
+        </h2>
+      </div>
+    );
+  }
 }
 
 export default App;
