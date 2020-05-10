b123922734a454b387c2485a3d6743e659c63cd7
prithvi16 <pvsukale@gmail.com>
2020-05-10 12:27:57 -0700

    more styling

--- src/App.js
+++ src/App.js
@@ -34,15 +34,51 @@ class App extends React.Component {
   }
   render() {
     const listItems = this.state.nearestLocations.map((location) => (
-      <li>{location["Center name"]}</li>
+      <li>
+        <a
+          href="#"
+          class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
+        >
+          <div class="flex items-center px-4 py-4 sm:px-6">
+            <div class="min-w-0 flex-1 flex items-center">
+              <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
+                <div>
+                  <div class="text-sm leading-5 font-medium text-indigo-600 truncate">
+                    {location["Center name"]}
+                  </div>
+                </div>
+                <div class="hidden md:block">
+                  <div>
+                    <div class="text-sm leading-5 text-gray-900">
+                      Applied on
+                      <time datetime="2020-01-07">January 7, 2020</time>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <svg
+                class="h-5 w-5 text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+        </a>
+      </li>
     ));
     return (
       <div className="text-center">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="max-w-3xl mx-auto">
-            <p class="text-xl font-semibold my-8">
-              COVID 19 testing locations
-            </p>
+            <p class="text-xl font-semibold my-8">COVID 19 testing locations</p>
             <button
               onClick={this.handleClick}
               type="button"
@@ -62,7 +98,10 @@ class App extends React.Component {
                 <Marker position={[location.latitude, location.longitude]} />
               ))}
             </Map>
-            <ol>{listItems}</ol>
+
+            <div class="bg-white shadow overflow-hidden sm:rounded-md my-8">
+              <ul>{listItems}</ul>
+            </div>
           </div>
         </div>
       </div>
