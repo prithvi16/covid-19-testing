d46b29bd099e6eeb7d9cdec6e52a77f0fbe0cfd1
prithvi16 <pvsukale@gmail.com>
2020-05-10 11:10:43 -0700

    Fixed styling

--- src/App.js
+++ src/App.js
@@ -38,27 +38,33 @@ class App extends React.Component {
     ));
     return (
       <div className="text-center">
-        <h1 class="text-xl font-semibold my-8">COVID 19 testing locations</h1>
-        <button onClick={this.handleClick} type="button" class="my-4 inline-flex items-center px-5 py-2 border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
-        Show nearest testing centers
-        </button>
-        <Map
-          center={[this.state.userLatitude, this.state.userLongitude]}
-          zoom={12}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-        {this.state.nearestLocations.map(location => (
-        <Marker
-          
-          position={[location.latitude, location.longitude]}
-        />
-      ))}
-        </Map>
-        <ol>{listItems}</ol>
-        
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="max-w-3xl mx-auto">
+            <p class="text-xl font-semibold my-8">
+              COVID 19 testing locations
+            </p>
+            <button
+              onClick={this.handleClick}
+              type="button"
+              class="my-4 inline-flex items-center px-5 py-2 border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+            >
+              Show nearest testing centers
+            </button>
+            <Map
+              center={[this.state.userLatitude, this.state.userLongitude]}
+              zoom={12}
+            >
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+              {this.state.nearestLocations.map((location) => (
+                <Marker position={[location.latitude, location.longitude]} />
+              ))}
+            </Map>
+            <ol>{listItems}</ol>
+          </div>
+        </div>
       </div>
     );
   }
